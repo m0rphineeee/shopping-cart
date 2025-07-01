ffbf8a7de44590c96672e2ea0d67087d34392773
xblitzzzk <203107663+xblitzzzk@users.noreply.github.com>
2025-07-01 10:40:09 -0300

    Adding new component

---
+++ react-app/src/components/ProductCart.jsx
@@ -0,0 +1,14 @@
+import "../styles/ProductCart.css"
+
+function ProductCart({product}) {
+    
+    return (
+        <div className="card">
+            <img src={product.imageUrl}/>
+            <p className='team-name'>{product.info.team}</p>
+            <p className='product-info'>{product.info.season} {product.info.condition}</p>
+        </div>
+    )
+}
+
+export default ProductCart
